3409c23ab8bbf05beb8bfab9434665f0b73cd705
cutiechi <chi@cutie.ooo>
2018-05-10 09:36:39 +0800

    feat(entity): add user entity to string, all and no args constructor

--- src/main/java/com/cutiechi/demo/model/entity/User.java
+++ src/main/java/com/cutiechi/demo/model/entity/User.java
@@ -1,15 +1,21 @@
 package com.cutiechi.demo.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * 用户
  *
  * @author Cutie Chi
  */
+@AllArgsConstructor
 @Getter
+@NoArgsConstructor
 @Setter
+@ToString
 public class User {
 
     /**
