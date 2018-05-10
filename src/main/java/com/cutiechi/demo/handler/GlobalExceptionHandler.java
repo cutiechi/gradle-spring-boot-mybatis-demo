9730bdbb7c5c997996c34444c86b26656c9c9918
cutiechi <chi@cutie.ooo>
2018-05-10 10:17:17 +0800

    feat(handler): add internal server error exception handle method

--- src/main/java/com/cutiechi/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cutiechi/demo/handler/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.cutiechi.demo.handler;
 
+import com.cutiechi.demo.exception.InternalServerErrorException;
+import com.cutiechi.demo.model.dto.JsonResponse;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -10,4 +14,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * 内部服务器错误异常处理方法
+     *
+     * @param exception 内部服务器错误异常
+     * @return JSON 响应
+     */
+    @ExceptionHandler(InternalServerErrorException.class)
+    public JsonResponse internalServerErrorException (InternalServerErrorException exception) {
+
+        // 返回约定状态码为 50000 并且附带异常信息的 JSON 响应
+        return new JsonResponse(50000, exception.getMessage());
+    }
 }
