6fb190dbe42ffb8fce5fdcbd0d95feea2075e0c5
cutiechi <chi@cutie.ooo>
2018-05-10 09:30:58 +0800

    feat(config): add application mapper scan config

--- src/main/java/com/cutiechi/demo/Application.java
+++ src/main/java/com/cutiechi/demo/Application.java
@@ -1,9 +1,12 @@
 package com.cutiechi.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cutiechi.demo.dao")
 public class Application {
 
     public static void main (String[] args) {
